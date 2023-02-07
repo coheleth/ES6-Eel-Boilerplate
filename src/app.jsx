a70dff0e316cba8de48550ef054f449bfb1d960b
coheleth <diogopiccirillo@gmail.com>
2023-02-06 23:27:53 -0300

    Added React-Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./layout";
+import { Home } from "./pages/home";
 
-export const App = () => <div>oie</div>;
+export const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
